2c884ff9c85f0e524105554770f341de4eff7fb6
Yusfuldev <owokoniran93@gmail.com>
2023-03-27 14:56:02 -0700

    update

--- print_b.c
+++ print_b.c
@@ -8,24 +8,25 @@
 
 int print_b(va_list b)
 {
-	unsigned int n, num[31];
-	int count = 0, i = 0;
-	char buf[32];
+	unsigned int n, num[sizeof(n) * 8], i = 0;
+	int count = 0;
+	char buf[sizeof(n) * 8 + 1];
 	char *p = buf;
 
 	n = va_arg(b, unsigned int);
-	for (; n != 0; i++)
+	for (; i < sizeof(n) * 8; i++)
 	{
 		num[i] = n % 2;
 		n /= 2;
 	}
 
-	while (i != 0)
+	while (num[i] != '\0')
 	{
 		i--;
 		*p++ = num[i] + '0';
 		count++;
 	}
+	*p = '\0';
 	write(1, buf, count);
 	va_end(b);
 	return (count);
