03057fbd8b3ced7fa5cfe0e16c687cff810be9fc
Chukwuemeka <124162009+Chukwuemeka9523@users.noreply.github.com>
2023-03-28 23:32:19 +0100

    Update print_p.c

--- print_p.c
+++ print_p.c
@@ -2,24 +2,49 @@
 #include <stdlib.h>
 
 /**
- * print_r - Prints a string in reverse
- * @r: string to print
+ * print_p - A function that prints an address
+ * @p: address to print
  * Return: number of printed characters
  */
-int print_r(va_list r)
+int print_p(va_list p)
 {
-	char *str;
-	int i = 0, counter = 0;
+	int counter = 0;
+	unsigned int a[16];
+	unsigned int i = 0, sum = 0;
+	unsigned long n, m = 1152921504606846976;
+	char *str = "(nil)";
 
-	str = va_arg(r, char *);
-	if (str == NULL)
-		str = ")llun(";
-	while (str[i])
-		i++;
-	for (i -= 1; i >= 0; i--)
+	n = va_arg(p, unsigned long);
+	if (n == 0)
 	{
-		_putchar(str[i]);
-		counter++;
+		for (; str[i]; i++)
+		{
+			_putchar(str[i]);
+			counter++;
+		}
+		return (counter);
+	}
+	_putchar('0');
+	_putchar('x');
+	counter = 2;
+
+	a[0] = n / m;
+	for (i = 1; i < 16; i++)
+	{
+		m /= 16;
+		a[i] = (n / m) % 16;
+	}
+	for (i = 0; i < 16; i++)
+	{
+		sum += a[i];
+		if (sum || i == 15)
+		{
+			if (a[i] < 10)
+				_putchar('0' + a[i]);
+			else
+				_putchar('0' + ('a' - ':') + a[i]);
+			counter++;
+		}
 	}
 	return (counter);
 }
